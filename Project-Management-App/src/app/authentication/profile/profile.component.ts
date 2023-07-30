b9679115c16e568c5f0e9ae36f97b4903d5e2a01
juliya <juliya@MacBook-Air-juliya.local>
2023-07-30 22:20:44 +0200

    feat: implement user editing and deleting functionality

--- Project-Management-App/src/app/authentication/profile/profile.component.ts
+++ Project-Management-App/src/app/authentication/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/app.models';
 import { AuthService } from 'src/app/services/auth.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,29 +12,61 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup<any> = new FormGroup({});
+  userName: string | null = null;
+  userLogin: string | null = null;
+  private isModalOpen = false;
 
   constructor(
     private router: Router,
     private authService: AuthService,
+    private modalService: ModalService,
     private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
-    const login = this.authService.getLogin();
+    this.userLogin = this.authService.getLogin();
+    this.userName = this.authService.getUserName();
     this.profileForm = this.formBuilder.group({
       name: ['', [Validators.required]],
-      login: [login, [Validators.required]],
+      login: [this.userLogin, [Validators.required]],
       password: ['', [Validators.required]],
     });
   }
 
   onSubmit(): void {
+    if (this.profileForm.invalid) {
+      return;
+    }
+
     const profileData: User = this.profileForm.value;
-    console.log(profileData);
-    alert('изменения успешно сохранены');
+    this.authService.updateUser(profileData).subscribe((resp) => {
+      if (resp) {
+        alert('Changes have been successfully saved');
+        this.profileForm.reset();
+      }
+    });
   }
 
   onCancel(): void {
     this.router.navigate(['/main-route']);
+  }
+
+  onDelete(): void {
+    if (!this.isModalOpen) {
+      this.isModalOpen = true;
+      const dialogRef = this.modalService.confirmationModalOpen(
+        'Are you sure you want to delete profile?'
+      );
+
+      dialogRef.afterClosed().subscribe((result) => {
+        this.isModalOpen = false;
+
+        if (result) {
+          this.authService.deleteUser().subscribe(() => {
+            this.authService.logout();
+          });
+        }
+      });
+    }
   }
 }
