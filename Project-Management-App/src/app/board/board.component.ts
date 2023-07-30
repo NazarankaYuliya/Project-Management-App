1a16414024a8827fec8b35887e1cb2bbd1a6e08a
juliya <juliya@MacBook-Air-juliya.local>
2023-07-30 22:14:31 +0200

    feat: add drag-n-drop

--- Project-Management-App/src/app/board/board.component.ts
+++ Project-Management-App/src/app/board/board.component.ts
@@ -1,9 +1,11 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { AddColumnModalComponent } from '../modal/add-column-modal/add-column-modal.component';
 
-import { Column, Task } from '../models/app.models';
+import { Column } from '../models/app.models';
 import { BackendService } from '../services/backend.service';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-board',
@@ -11,25 +13,100 @@ import { BackendService } from '../services/backend.service';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
-  public boardId: string | null = null;
-  public boardTitle: string | null = null;
-  public columns$!: Observable<Column[]>;
-  public columnsId: string | null = null;
-  public tasks$!: Observable<Task[]>;
+  private isModalOpen = false;
+  boardId: string | null = null;
+  boardTitle: string | null = null;
+  columns: Column[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private backendService: BackendService
+    private backendService: BackendService,
+    private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.boardId = params.get('id');
       if (this.boardId) {
-        this.backendService.setBoardId(this.boardId);
         this.backendService
           .getBoardById(this.boardId)
           .subscribe((board) => (this.boardTitle = board.title));
+
+        this.backendService.getAllColumns(this.boardId).subscribe((columns) => {
+          this.columns = columns;
+          this.columns.sort((a, b) => a.order - b.order);
+        });
+      }
+    });
+  }
+
+  createColumn() {
+    if (!this.isModalOpen) {
+      this.isModalOpen = true;
+      const dialogRef = this.modalService.creationModalOpen(
+        AddColumnModalComponent
+      );
+
+      dialogRef.afterClosed().subscribe((result) => {
+        this.isModalOpen = false;
+
+        if (this.boardId && result && result.submitted) {
+          const columnData: Column = {
+            title: result.data.title,
+            order: this.columns.length + 1,
+          };
+
+          this.backendService.createColumn(this.boardId, columnData).subscribe(
+            (responce) => {
+              this.columns.push(responce);
+              dialogRef.close();
+            },
+            (error) => {
+              console.error(error);
+            }
+          );
+        }
+      });
+    }
+  }
+
+  handleColumnDeleted(column: Column) {
+    const index = this.columns.findIndex((c) => c._id === column._id);
+    if (index !== -1) {
+      this.columns.splice(index, 1);
+      this.columns.forEach((column, i) => (column.order = i + 1));
+    }
+  }
+
+  handleColumnUpdated(updatedColumn: Column) {
+    const existingColumn = this.columns.find(
+      (c) => c._id === updatedColumn._id
+    );
+    if (existingColumn) {
+      existingColumn.title = updatedColumn.title;
+      existingColumn.order = updatedColumn.order;
+    }
+  }
+
+  drop(event: CdkDragDrop<Column[]>) {
+    if (event.previousIndex === event.currentIndex) {
+      return;
+    }
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+    this.updateColumnOrder();
+  }
+
+  updateColumnOrder() {
+    this.columns.forEach((column, index) => {
+      if (this.boardId && column._id) {
+        this.backendService
+          .updateColumn(this.boardId, column._id, {
+            title: column.title,
+            order: index + 1,
+          })
+          .subscribe((response) => {
+            this.handleColumnUpdated(response);
+          });
       }
     });
   }
--- Project-Management-App/src/app/board/column/column.component.ts
+++ Project-Management-App/src/app/board/column/column.component.ts
@@ -1,9 +1,13 @@
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
-import { Observable } from 'rxjs';
-import { AddColumnModalComponent } from 'src/app/modal/add-column-modal/add-column-modal.component';
 import { AddTaskModalComponent } from 'src/app/modal/add-task-modal/add-task-modal.component';
 import { UpdateColumnModalComponent } from 'src/app/modal/update-column-modal/update-column-modal.component';
 import { Column, Task } from 'src/app/models/app.models';
+import { AuthService } from 'src/app/services/auth.service';
 import { BackendService } from 'src/app/services/backend.service';
 import { ModalService } from 'src/app/services/modal.service';
 
@@ -13,56 +17,39 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./column.component.scss'],
 })
 export class ColumnComponent implements OnInit {
+  @Input() column!: Column;
   @Input() boardId!: string | null;
 
-  columns$!: Observable<Column[]>;
+  @Output() columnDeleted: EventEmitter<Column> = new EventEmitter<Column>();
+
+  @Output() columnUpdated: EventEmitter<Column> = new EventEmitter<Column>();
+
   private isModalOpen = false;
+  tasks: Task[] = [];
+  test: Task[] = [
+    { title: 'test', order: 0, description: 'test', userId: 'test', users: [] },
+  ];
 
   constructor(
     private backendService: BackendService,
+    private authService: AuthService,
     private modalService: ModalService
   ) {}
 
   ngOnInit(): void {
-    if (this.boardId) {
-      this.columns$ = this.backendService.getAllColumns(this.boardId);
+    if (this.boardId && this.column._id) {
+      this.getTasksForColumn(this.boardId, this.column._id);
     }
   }
 
-  addColumn() {
-    if (!this.isModalOpen) {
-      this.isModalOpen = true;
-      const dialogRef = this.modalService.creationModalOpen(
-        AddColumnModalComponent
-      );
-      dialogRef.afterClosed().subscribe(() => {
-        this.isModalOpen = false;
-      });
-    }
+  getTasksForColumn(boardId: string, columnId: string) {
+    this.backendService.getAllTasks(boardId, columnId).subscribe((tasks) => {
+      this.tasks = tasks;
+      this.tasks.sort((a, b) => a.order - b.order);
+    });
   }
 
-  updateColumn(columnId: string, columnTitle: string, columnOrder: number) {
-    if (!this.isModalOpen) {
-      this.isModalOpen = true;
-      const dialogRef = this.modalService.creationModalOpen(
-        UpdateColumnModalComponent,
-        {
-          boardId: this.boardId,
-          columnId: columnId,
-          columnTitle: columnTitle,
-          columnOrder: columnOrder,
-        }
-      );
-      dialogRef.afterClosed().subscribe(() => {
-        if (this.boardId) {
-          this.columns$ = this.backendService.getAllColumns(this.boardId);
-        }
-        this.isModalOpen = false;
-      });
-    }
-  }
-
-  deleteColumn(columnId: string, boardId: string) {
+  deleteColumn() {
     if (!this.isModalOpen) {
       this.isModalOpen = true;
       const dialogRef = this.modalService.confirmationModalOpen(
@@ -70,13 +57,144 @@ export class ColumnComponent implements OnInit {
       );
 
       dialogRef.afterClosed().subscribe((result) => {
-        if (result) {
-          this.backendService.deleteColumn(columnId, boardId).subscribe(() => {
-            this.columns$ = this.backendService.columns$;
-          });
-        }
         this.isModalOpen = false;
+
+        if (result && this.boardId && this.column._id) {
+          this.backendService
+            .deleteColumn(this.boardId, this.column._id)
+            .subscribe(() => {
+              this.columnDeleted.emit(this.column);
+            });
+        }
       });
     }
+  }
+
+  updateColumn() {
+    if (!this.isModalOpen) {
+      this.isModalOpen = true;
+      const dialogRef = this.modalService.creationModalOpen(
+        UpdateColumnModalComponent
+      );
+
+      dialogRef.afterClosed().subscribe((result) => {
+        this.isModalOpen = false;
+
+        if (result && result.submitted) {
+          const updatedData: Column = {
+            title: result.data.title,
+            order: this.column.order,
+          };
+          if (this.boardId && this.column._id) {
+            this.backendService
+              .updateColumn(this.boardId, this.column._id, updatedData)
+              .subscribe(
+                (response) => {
+                  this.columnUpdated.emit(response);
+                  dialogRef.close();
+                },
+                (error) => {
+                  console.error(error);
+                }
+              );
+          }
+        }
+      });
+    }
+  }
+
+  addTask(): void {
+    if (!this.isModalOpen) {
+      this.isModalOpen = true;
+      const dialogRef = this.modalService.creationModalOpen(
+        AddTaskModalComponent
+      );
+
+      dialogRef.afterClosed().subscribe((result) => {
+        this.isModalOpen = false;
+        const userId = this.authService.getUserId();
+
+        if (result && result.submitted && userId) {
+          const taskData: Task = {
+            title: result.data.title,
+            order: this.tasks.length + 1,
+            description: result.data.description,
+            userId: userId,
+            users: [],
+          };
+
+          if (this.boardId && this.column._id) {
+            this.backendService
+              .createTask(this.boardId, this.column._id, taskData)
+              .subscribe(
+                (response) => {
+                  this.tasks.push(response);
+                  dialogRef.close();
+                },
+                (error) => {
+                  console.error(error);
+                }
+              );
+          }
+        }
+      });
+    }
+  }
+
+  handleTaskDeleted(task: Task) {
+    const index = this.tasks.findIndex((c) => c._id === task._id);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+      this.tasks.forEach((task, i) => (task.order = i + 1));
+    }
+  }
+  handleTaskUpdated(updatedTask: Task) {
+    const existingTask = this.tasks.find((c) => c._id === updatedTask._id);
+    if (existingTask) {
+      existingTask.title = updatedTask.title;
+      existingTask.description = updatedTask.description;
+      existingTask.order = updatedTask.order;
+    }
+  }
+
+  drop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer === event.container) {
+      if (event.previousIndex === event.currentIndex) {
+        return;
+      }
+
+      moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+      this.updateTaskOrder();
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+
+      const targetColumnId = this.column._id;
+
+      console.log(targetColumnId);
+    }
+  }
+
+  updateTaskOrder() {
+    this.tasks.forEach((task, index) => {
+      if (this.boardId && this.column._id && task._id) {
+        this.backendService
+          .updateTask(this.boardId, this.column._id, task._id, {
+            title: task.title,
+            order: index + 1,
+            description: task.description,
+            columnId: this.column._id,
+            userId: this.authService.getUserId(),
+            users: [],
+          })
+          .subscribe((response) => {
+            this.handleTaskUpdated(response);
+          });
+      }
+    });
   }
 }
